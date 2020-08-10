e5c21524e79f91e79aec988e97523c17e03b08d3
Aman Bal <amandeep1420@gmail.com>
2020-08-10 19:14:16 +0000

    removed duplicate directories getting_started and the_basics

--- getting_started/pry_notes.rb
+++
@@ -1,6 +0,0 @@
-require "pry" #when you want to use pry to debug, you must insert this at the beginning of the code to be debugged...
-
-puts "meow mix"
-
-binding.pry #...and this at the end of the code to be debugged
-

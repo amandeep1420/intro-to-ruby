c24abde76d252fea558f936fa59ce3c58ad4749e
Aman Bal <amandeep1420@gmail.com>
2020-08-29 22:59:27 +0000

    ...

--- 9_more_stuff/2_ruby_standard_classes.rb
+++ 9_more_stuff/2_ruby_standard_classes.rb
@@ -17,3 +17,34 @@ Math.sqrt(400)
 
 
 =begin
+Nice. Another scenario: maybe you need to use Pi for something. See below - the Pi being used is a 'constant' that comes with the "Math" module. 
+It's okay to not know what constants are yet or the syntax being used - just understand the concept for now. We're using Ruby's built-in libraries 
+to solve our problems instead of building our own solutions from scratch every time. 
+=end
+
+
+Math::PI
+=> 3.141592653589793
+
+
+=begin
+Something a little more involved: what if you wanted to calculate the specific day July 4th occurred on in 2008? Easy-peasy; you can use Ruby's 
+built-in "Time" class. See below:
+=end
+
+
+t = Time.new(2008, 7, 4)
+=> 2008-07-04 00:00:00 -0400
+irb :004 > t.monday?
+=> false
+irb :005 > t.friday?
+=> true
+
+
+=begin
+You're starting to see the power of using Ruby's standard classes. There is no use reinventing the wheel when you have a rich resource to 
+accomplish tasks and solve problems that many have contended with before.
+
+*Note: why "Math.sqrt" and not "math.sqrt"? Because sqrt is a 'class method' and not an 'instance method'. We've mostly dealt with instance 
+methods so far, hence the possible confusion. Just use the syntax we introduced for now. We'll talk about the differences between class and 
+instance methods later in the book when we introduce OOP (Object Oriented Programming) concepts.
